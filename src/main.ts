49083438120208018f1dd0728e5e1cc18f15e3bc
EstebanM159 <estebanm91231@gmail.com>
2025-09-06 21:47:36 -0300

    Auto-ping borrado

--- src/main.ts
+++ src/main.ts
@@ -27,22 +27,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const SELF_URL =
-    process.env.SELF_URL ||
-    `https://nest-teslo-shop-backend-30bc.onrender.com/api`;
-  // 👆 Crea la variable SELF_URL en Render con la URL real de tu app
-
-  if (process.env.RENDER) {
-    const intervalId = setInterval(async () => {
-      try {
-        const res = await fetch(SELF_URL);
-        logger.log(`Ping enviado: ${res.status}`);
-      } catch (err) {
-        logger.error(`Error en autopinger: ${err.message}`);
-      }
-    }, 10 * 60 * 1000); // cada 14 minutos
-  }
-
   await app.listen(process.env.PORT);
   logger.log(`App running on port ${process.env.PORT}`);
 }
